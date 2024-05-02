4a60e062b7da72de6b9c8722e80894590980b578
alexfatalix <alexfatalix@gmail.com>
2024-05-02 14:16:38 +0300

    added login tests

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,9 +1,21 @@
 const { test, expect } = require('../utils/fixtures');
+import { fakerUK } from '@faker-js/faker'
 
 test.beforeEach(async ({ loginPage }) => {
-  await loginPage.goto('')
+  await loginPage.goto()
   })
 
-test('Get started link', async ({ page }) => {
-    await expect(page).toHaveURL("https://slotoking.ua");
+  test('Successful login', async ({ page, loginPage }) => {
+    await loginPage.login(process.env.REGULAR_USER1_EMAIL, process.env.REGULAR_USER1_PASSWORD)
+    await page.waitForSelector(loginPage.notificationSelector)
+  })
+
+  test('Unsuccessful login with wrong password', async ({ loginPage }) => {
+    await loginPage.login(process.env.REGULAR_USER1_EMAIL, fakerUK.internet.password())
+    await expect(loginPage.passwordValidationMessage).toContainText("Невірний пароль")
+  })
+
+  test('Unsuccessful login with wrong email', async ({ loginPage }) => {
+    await loginPage.login(fakerUK.internet.email(), fakerUK.internet.password())
+    await expect(loginPage.passwordValidationMessage).toContainText("Обліковий запис не знайдений")
   })
